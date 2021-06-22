df3ab3693aa8911ebc9f30b22ea8dd42a922ff5d
Taylor Britton <tbrit10@gmail.com>
2021-06-21 18:16:41 -0700

    Add text formatting extensions

---
+++ TriviaBot/Extensions/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaBot.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string PadBoth(this string str, int length)
+        {
+            int spaces = length - str.Length;
+            int padLeft = spaces / 2 + str.Length;
+            return str.PadLeft(padLeft).PadRight(length);
+        }
+
+        public static string PadOffcenter(this string toPad, int centerLength, int totalLength)
+        {
+            int spaces = centerLength - toPad.Length;
+            int padRight = totalLength - centerLength;
+            return toPad.PadLeft(centerLength/2).PadRight(padRight);
+        }
+    }
+}
